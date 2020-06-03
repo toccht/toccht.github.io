6e3716b8d62a9ac813407d3fb132c52009868285
toccht <toccht@rpi.edu>
2020-06-02 23:37:08 -0400

    updated

--- src/projects.js
+++ src/projects.js
@@ -2,19 +2,24 @@ $(document).ready(function(){
 
 	$.ajax({
 		type: "GET",
-   	 	url: "projects.json",
-   	 	dataType: "json",
-   	 	success: function(responseData, status){
-   	 		var output = "<ul>";
-   	 		$.each(responseData.menuItem, function(i, item) {
-   	 			output += '<p>' + item.menuName + '</p>';
-   	 			output += '<li><a href="' + item.menuURL + '">' + item.menuDesc + '</a></li>';
-   	 		});
-   	 		output += "</ul>";
-   	 		$('#projectsOutput').html(output);
+   	url: "json/projects.json",
+   	dataType: "json",
+
+   	success: function(responseData, status){
+   	 	output = "<ul>";
+   	 	$.each(responseData.menuItem, function(i, menuItem) {
+   	 		output += '<li>' + menuItem.menuName + '</li>';
+   	 		//output += '<li>' + menuItem.menuDesc + '</li>';
+   	 	});
+
+   	 	output += "</ul>";
+   	 	$('#projectsOutput').html(output);
+
  		}, error: function(msg) {
 				// there was a problem
 			alert("There was a problem: " + msg.status + " " + msg.statusText);
 		}
+
 	});
+
 });
